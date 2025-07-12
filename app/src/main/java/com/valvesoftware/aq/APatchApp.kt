72d243c4f892287d3447de53f7cf18802e44badb
hasandramali <93338836+hasandramali@users.noreply.github.com>
2025-07-13 01:53:20 +0300

    remove apksignature check

--- app/src/main/java/com/valvesoftware/aq/APatchApp.kt
+++ app/src/main/java/com/valvesoftware/aq/APatchApp.kt
@@ -256,7 +256,7 @@ class APApplication : Application(), Thread.UncaughtExceptionHandler {
             exitProcess(0)
         }
 
-        if (!BuildConfig.DEBUG && !verifyAppSignature("1x2twMoHvfWUODv7KkRRNKBzOfEqJwRKGzJpgaz18xk=")) {
+        /*if (!BuildConfig.DEBUG && !verifyAppSignature("1x2twMoHvfWUODv7KkRRNKBzOfEqJwRKGzJpgaz18xk=")) {
             while (true) {
                 val intent = Intent(Intent.ACTION_DELETE)
                 intent.data = "package:$packageName".toUri()
@@ -265,7 +265,7 @@ class APApplication : Application(), Thread.UncaughtExceptionHandler {
                 startActivity(intent)
                 exitProcess(0)
             }
-        }
+        }*/
 
         // TODO: We can't totally protect superkey from be stolen by root or LSPosed-like injection tools in user space, the only way is don't use superkey,
         // TODO: 1. make me root by kernel
